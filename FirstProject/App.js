8b10aa3d9c7f4753b8ee1a28b6dae728442b8d32
tmmoond8 <tmmoond8@gmail.com>
2021-11-23 12:27:12 +0900

    🎉 RN 공부 시작

---
+++ FirstProject/App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello React!</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
