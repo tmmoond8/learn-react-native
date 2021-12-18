23844c88db195ace9f3e711a7f06222376677ee6
tmmoond8 <tmmoond8@gmail.com>
2021-12-18 15:31:31 +0900

    ✨ feed 리스트 렌더링

--- DayLog/screens/FeedScreen.js
+++ DayLog/screens/FeedScreen.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 import FloatingWriteButton from '../components/FloatingWriteButton';
+import {useLogContext} from '../contexts/LogContext';
+import FeedList from '../components/FeedList';
 
 export default function FeedScreen() {
+  const {logs} = useLogContext();
   return (
     <View style={styles.block}>
+      <FeedList logs={logs} />
       <FloatingWriteButton />
     </View>
   );
