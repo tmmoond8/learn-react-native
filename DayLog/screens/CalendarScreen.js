783c2659625ae561424591b567522980607c3cb1
tmmoond8 <tmmoond8@gmail.com>
2021-12-18 15:51:58 +0900

    ✨ 기본적인 애니메이션 예시

--- DayLog/screens/CalendarScreen.js
+++ DayLog/screens/CalendarScreen.js
@@ -1,10 +1,117 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {Animated, Button, View, StyleSheet} from 'react-native';
 
 export default function CalendarScreen() {
-  return <View style={styles.block}></View>;
+  return (
+    <View style={styles.block}>
+      {/* <FadeInAndOut /> */}
+      {/* <SlideLeftAndRight /> */}
+      <SlideLeftAndRightWithFade />
+    </View>
+  );
+}
+
+function SlideLeftAndRightWithFade() {
+  const animation = React.useRef(new Animated.Value(0)).current;
+  const [enabled, setEnabled] = React.useState(false);
+
+  React.useEffect(() => {
+    Animated.timing(animation, {
+      toValue: enabled ? 1 : 0,
+      useNativeDriver: true,
+    }).start();
+  }, [enabled, animation]);
+
+  return (
+    <View>
+      <Animated.View
+        style={[
+          styles.rectangle,
+          {
+            transform: [
+              {
+                translateX: animation.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [0, 150],
+                }),
+              },
+            ],
+            opacity: animation.interpolate({
+              inputRange: [0, 1],
+              outputRange: [1, 0],
+            }),
+          },
+        ]}
+      />
+      <Button
+        title="Toggle"
+        onPress={() => {
+          setEnabled(!enabled);
+        }}
+      />
+    </View>
+  );
+}
+
+function SlideLeftAndRight() {
+  const animation = React.useRef(new Animated.Value(0)).current;
+  const [enabled, setEnabled] = React.useState(false);
+
+  React.useEffect(() => {
+    Animated.timing(animation, {
+      toValue: enabled ? 150 : 0,
+      useNativeDriver: true,
+    }).start();
+  }, [enabled, animation]);
+
+  return (
+    <View>
+      <Animated.View
+        style={[styles.rectangle, {transform: [{translateX: animation}]}]}
+      />
+      <Button
+        title="Toggle"
+        onPress={() => {
+          setEnabled(!enabled);
+        }}
+      />
+    </View>
+  );
+}
+
+function FadeInAndOut() {
+  const animation = React.useRef(new Animated.Value(1)).current;
+
+  return (
+    <View>
+      <Animated.View style={[styles.rectangle, {opacity: animation}]} />
+      <Button
+        title="FadeIn"
+        onPress={() => {
+          Animated.timing(animation, {
+            toValue: 1,
+            useNativeDriver: true,
+          }).start();
+        }}
+      />
+      <Button
+        title="FadeOut"
+        onPress={() => {
+          Animated.timing(animation, {
+            toValue: 0,
+            useNativeDriver: true,
+          }).start();
+        }}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   block: {},
+  rectangle: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'black',
+  },
 });
