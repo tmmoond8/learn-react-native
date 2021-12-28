3a6840ea5a8c383f13f8899670f614f08af89845
tmmoond8 <tmmoond8@gmail.com>
2021-12-28 21:17:33 +0900

    ➕ firebase 모듈 추가

---
+++ PublicGalleryTamm/src/screens/RootStack.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+export default function RootStack() {
+  return <Stack.Navigator>화면 추가 예정</Stack.Navigator>;
+}
