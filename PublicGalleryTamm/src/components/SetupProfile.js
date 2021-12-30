6f275dc99105c1e95ac5faab708b4705df5ee175
tmmoond8 <tmmoond8@gmail.com>
2021-12-31 01:40:49 +0900

    ✨ firebase 이미지 업로드

--- PublicGalleryTamm/src/components/SetupProfile.js
+++ PublicGalleryTamm/src/components/SetupProfile.js
@@ -1,7 +1,15 @@
 import {useNavigation, useRoute} from '@react-navigation/native';
 import React from 'react';
-import {Image, Pressable, View, StyleSheet, Platform} from 'react-native';
+import {
+  Image,
+  Pressable,
+  ActivityIndicator,
+  View,
+  StyleSheet,
+  Platform,
+} from 'react-native';
 import {launchImageLibrary} from 'react-native-image-picker';
+import storage from '@react-native-firebase/storage';
 import {useUserContext} from '../contexts/UserContext';
 import {signOut} from '../libs/auth';
 import {createUser} from '../libs/users';
@@ -13,15 +21,34 @@ export default function SetupProfile() {
   const navigation = useNavigation();
   const {setUser} = useUserContext();
   const [response, setReponse] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
 
   const {params} = useRoute();
   const {uid} = params || {};
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
+    setLoading(true);
+    let photoURL = null;
+
+    if (response) {
+      const asset = response.assets[0];
+      const extension = asset.fileName.split('.').pop();
+      const reference = storage().ref(`/profile/${uid}.${extension}`);
+
+      if (Platform.OS === 'android') {
+        await reference.putString(asset.base64, 'base64', {
+          contentType: asset.type,
+        });
+      } else {
+        await reference.putFile(asset.uri);
+      }
+      photoURL = response ? await reference.getDownloadURL() : null;
+    }
+
     const user = {
       id: uid,
       displayName,
-      photoURL: null,
+      photoURL,
     };
     createUser(user);
     setUser(user);
@@ -70,10 +97,18 @@ export default function SetupProfile() {
           onSubmitEditing={handleSubmit}
           returnKeyType="next"
         />
-        <View style={styles.buttons}>
-          <CustomButton title="다음" onPress={handleSubmit} hasMarginBottom />
-          <CustomButton title="취소" onPress={handleCancel} theme="secondary" />
-        </View>
+        {loading ? (
+          <ActivityIndicator size={32} color="#6200ee" style={styles.spinner} />
+        ) : (
+          <View style={styles.buttons}>
+            <CustomButton title="다음" onPress={handleSubmit} hasMarginBottom />
+            <CustomButton
+              title="취소"
+              onPress={handleCancel}
+              theme="secondary"
+            />
+          </View>
+        )}
       </View>
     </View>
   );
@@ -91,6 +126,9 @@ const styles = StyleSheet.create({
     borderRadius: 64,
     width: 128,
     height: 128,
+  },
+  spinner: {
+    marginTop: 24,
   },
   form: {
     marginTop: 16,
--- PublicGalleryTamm/src/screens/MainTab.js
+++ PublicGalleryTamm/src/screens/MainTab.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {Image, View, Text, StyleSheet} from 'react-native';
 import {useUserContext} from '../contexts/UserContext';
 
 export default function MainTab() {
   const {user} = useUserContext();
   return (
     <View style={styles.block}>
+      {user.photoURL && (
+        <Image
+          source={{uri: user.photoURL}}
+          style={styles.image}
+          resizeMode="cover"
+        />
+      )}
       <Text style={styles.text}>Hello, {user.displayName}</Text>
     </View>
   );
@@ -16,6 +23,12 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: 128,
+    height: 128,
+    marginBottom: 16,
+    borderRadius: 64,
   },
   text: {
     fontSize: 24,
