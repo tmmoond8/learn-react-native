3e2a5158d814923002c761828c8725745affb68c
tmmoond8 <tmmoond8@gmail.com>
2021-12-31 15:07:11 +0900

    ✨ 카메라 버튼 가운데 정렬

--- PublicGalleryTamm/src/screens/MainTab.js
+++ PublicGalleryTamm/src/screens/MainTab.js
@@ -1,55 +1,51 @@
 import React from 'react';
-import {Image, View, Text, StyleSheet} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {useUserContext} from '../contexts/UserContext';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import HomeStack from './HomeStack';
 import MyProfileStack from './MyProfileStack';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import CameraButton from '../components/CameraButton';
 
 const Tab = createBottomTabNavigator();
 
 export default function MainTab() {
-  const {user} = useUserContext();
   return (
-    <Tab.Navigator
-      screenOptions={{
-        headerShown: false,
-        tabBarShowLabel: false,
-        tabBarActiveTintColor: '#6200ee',
-      }}>
-      <Tab.Screen
-        name="HomeStack"
-        component={HomeStack}
-        options={{
-          tabBarIcon: ({color}) => <Icon name="home" size={24} color={color} />,
-        }}
-      />
-      <Tab.Screen
-        name="MyProfileStack"
-        component={MyProfileStack}
-        options={{
-          tabBarIcon: ({color}) => (
-            <Icon name="person" size={24} color={color} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
+    <>
+      <View style={styles.block}>
+        <Tab.Navigator
+          screenOptions={{
+            headerShown: false,
+            tabBarShowLabel: false,
+            tabBarActiveTintColor: '#6200ee',
+          }}>
+          <Tab.Screen
+            name="HomeStack"
+            component={HomeStack}
+            options={{
+              tabBarIcon: ({color}) => (
+                <Icon name="home" size={24} color={color} />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="MyProfileStack"
+            component={MyProfileStack}
+            options={{
+              tabBarIcon: ({color}) => (
+                <Icon name="person" size={24} color={color} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </View>
+      <CameraButton />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   block: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  image: {
-    width: 128,
-    height: 128,
-    marginBottom: 16,
-    borderRadius: 64,
-  },
-  text: {
-    fontSize: 24,
+    zIndex: 0,
   },
 });
