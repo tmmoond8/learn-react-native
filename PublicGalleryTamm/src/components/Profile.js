c0e4bd4ab8740fda361ecb33b6045aa1959e39ad
tmmoond8 <tmmoond8@gmail.com>
2022-01-01 15:00:00 +0900

    프로필에 포스트 페이지네이션

--- PublicGalleryTamm/src/components/Profile.js
+++ PublicGalleryTamm/src/components/Profile.js
@@ -4,9 +4,10 @@ import {
   FlatList,
   Text,
   View,
+  RefreshControl,
   StyleSheet,
 } from 'react-native';
-import {getPosts} from '../libs/posts';
+import {getPosts, getNewerPosts, getOlderPosts, PAGE_SIZE} from '../libs/posts';
 import {getUser} from '../libs/users';
 import {getImageUrl} from '../libs/utils';
 import Avatar from './Avatar';
@@ -15,6 +16,36 @@ import PostGridItem from './PostGridItem';
 export default function Profile({userId}) {
   const [user, setUser] = React.useState(null);
   const [posts, setPosts] = React.useState(null);
+
+  const [noMorePost, setNoMorePost] = React.useState(false);
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const handleLoadMore = async () => {
+    if (noMorePost || !posts || posts.length < PAGE_SIZE) {
+      return;
+    }
+    const lastPost = posts[posts.length - 1];
+    const olderPosts = await getOlderPosts(lastPost, userId);
+
+    if (olderPosts.length < PAGE_SIZE) {
+      setNoMorePost(true);
+    }
+    setPosts(posts.concat(olderPosts));
+  };
+
+  const handleRefresh = async () => {
+    if (!posts || posts.length === 0 || refreshing) {
+      return;
+    }
+    const firstPost = posts[0];
+    setRefreshing(true);
+    const newerPosts = await getNewerPosts(firstPost.id, userId);
+    setRefreshing(false);
+    if (newerPosts.length === 0) {
+      return;
+    }
+    setPosts(newerPosts.concat(posts));
+  };
 
   const renderItem = React.useMemo(() => {
     return ({item}) => <PostGridItem post={item} />;
@@ -47,6 +78,20 @@ export default function Profile({userId}) {
           <Text style={styles.userName}>{user.displayName}</Text>
         </View>
       }
+      onEndReached={handleLoadMore}
+      onEndReachedThreshold={0.25}
+      ListFooterComponent={
+        !noMorePost && (
+          <ActivityIndicator
+            style={styles.bottomSpinner}
+            size={32}
+            color="#6200ee"
+          />
+        )
+      }
+      refreshControl={
+        <RefreshControl onRefresh={handleRefresh} refreshing={refreshing} />
+      }
     />
   );
 }
@@ -65,5 +110,8 @@ const styles = StyleSheet.create({
     marginTop: 8,
     fontSize: 24,
     color: '#424242',
+  },
+  bottomSpinner: {
+    height: 128,
   },
 });
